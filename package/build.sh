5949808b0ea184316f1850f341a46dbad4da3b6b
Patrick Stephens <pat@calyptia.com>
2023-05-19 15:32:38 +0100

    package: update for templating
    Signed-off-by: Patrick Stephens <pat@calyptia.com>

--- package/build.sh
+++ package/build.sh
@@ -7,8 +7,18 @@ if ! command -v nfpm &> /dev/null; then
     exit 1
 fi
 
+# The architecture to build for
+export ARCH=${ARCH:-amd64}
+# The package version
+export VERSION=${VERSION:-1.0.0}
+# Where to put the packages
 OUTPUT_DIR=${OUTPUT_DIR:-$SCRIPT_DIR/output}
 
+# Allow us to cope with any architecture via simple substitution
+# shellcheck disable=SC2016
+envsubst '$ARCH,$VERSION' < "$SCRIPT_DIR/nfpm-template.yaml" > "$SCRIPT_DIR/nfpm.yaml"
+
+# Need to be in the right directory for relative paths in the template
 pushd "$SCRIPT_DIR"
 nfpm package --config "$SCRIPT_DIR/nfpm.yaml" --packager apk --target "$OUTPUT_DIR"
 nfpm package --config "$SCRIPT_DIR/nfpm.yaml" --packager deb --target "$OUTPUT_DIR"
